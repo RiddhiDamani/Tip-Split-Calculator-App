3d04a85370281452879b262af0bab2734a5fdd79
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-01 22:12:23 -0500

    Added Textviews

--- app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
+++ app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
@@ -1,16 +1,17 @@
 package com.riddhidamani.tipsplit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+    }
 
 }
