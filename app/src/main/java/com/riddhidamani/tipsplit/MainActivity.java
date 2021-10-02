d4dc90b8a9e5bbf7b47ad7a7a7f8ab1008c9a9b9
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-02 11:41:21 -0500

    Added Clear functionality

--- app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
+++ app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
@@ -8,21 +8,43 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText inputValue;
-    private TextView screenValue;
+    private EditText billTotalValue;
+    private EditText numOfPplTxtValue;
+
+
+    private TextView totAmtPerPersonTxtValue;
+    private TextView tipAmtTxtValue;
+    private TextView totWithTipTxtValue;
+    private TextView overageTxtValue;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputValue = findViewById(R.id.editTextNumberDecimal);
-        screenValue = findViewById(R.id.totAmtPerPersonTxt);
+        billTotalValue = findViewById(R.id.billTotal);
+        tipAmtTxtValue = findViewById(R.id.tipAmtTxt);
+        totWithTipTxtValue = findViewById(R.id.totWithTipTxt);
+        numOfPplTxtValue = findViewById(R.id.numOfPplTxt);
+        totAmtPerPersonTxtValue = findViewById(R.id.totAmtPerPersonTxt);
+        overageTxtValue = findViewById(R.id.overageTxt);
+
     }
 
     public void getValue(View v) {
-        String value = inputValue.getText().toString();
-        screenValue.setText(value);
+        String value = billTotalValue.getText().toString();
+        totAmtPerPersonTxtValue.setText(value);
+    }
+
+    public void clearAllFields(View v) {
+        billTotalValue.setText("");
+        tipAmtTxtValue.setText("");
+        totWithTipTxtValue.setText("");
+        numOfPplTxtValue.setText("");
+        totAmtPerPersonTxtValue.setText("");
+        overageTxtValue.setText("");
     }
 
 }
