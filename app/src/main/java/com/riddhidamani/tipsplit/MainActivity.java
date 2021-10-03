3e2634d73c48feaced7dbce116b5d5beef7ffa59
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-02 20:08:10 -0500

    Added Tip % Radio Buttons

--- app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
+++ app/src/main/java/com/riddhidamani/tipsplit/MainActivity.java
@@ -2,22 +2,23 @@ package com.riddhidamani.tipsplit;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private EditText billTotalValue;
     private EditText numOfPplTxtValue;
-
 
     private TextView totAmtPerPersonTxtValue;
     private TextView tipAmtTxtValue;
     private TextView totWithTipTxtValue;
     private TextView overageTxtValue;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
     public void getValue(View v) {
         String value = billTotalValue.getText().toString();
         totAmtPerPersonTxtValue.setText(value);
+    }
+
+    public void clickGroup1(View v) {
+        String label = ((RadioButton)v).getText().toString();
+        if(v.getId() == R.id.rb1) {
+
+        }
+        else if(v.getId() == R.id.rb2) {
+
+        }
+        else if(v.getId() == R.id.rb3) {
+
+        }
+        else if(v.getId() == R.id.rb3) {
+
+        }
+        else {
+
+            Log.d(TAG, "ClickGroup1: Unexpected radio button click: " + label);
+        }
     }
 
     public void clearAllFields(View v) {
